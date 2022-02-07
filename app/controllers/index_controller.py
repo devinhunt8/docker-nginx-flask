345e544fe5d551f06180419eeaa1640c205e4f83
Devin Hunt <60887893+devinhunt8@users.noreply.github.com>
2022-02-07 12:16:40 -0500

    Devin Hunt

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "kwilliam"
+        name = "Devin Hunt"
         return render_template('index.html', name=name)
